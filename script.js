3c3f42f59b4dcf1edbac57b5bffca08ccee6cc3a
dariaasty <dariaasty@users.noreply.github.com>
2026-03-31 10:40:21 +0500

    Upload Anastasia portfolio site

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.querySelector("#theme-toggle");
-
-toggleButton?.addEventListener("click", () => {
-  const body = document.body;
-  const nextTheme =
-    body.dataset.theme === "evening" ? "daylight" : "evening";
-
-  body.dataset.theme = nextTheme;
-});
